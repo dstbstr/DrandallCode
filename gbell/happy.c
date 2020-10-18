0bc7f09324a7938404d4544884479402034313c6
Greg Bell <gdb3941@gmail.com>
2020-10-18 11:04:03 -0700

    Happy Birthday Ternary Version

--- gbell/happy.c
+++ gbell/happy.c
@@ -3,11 +3,12 @@
 int
 main(int argc,char *argv[])
 {
-    int i = 0;
+    int i = 4;
 
-    while (i < 4) {
-        argc == 2 ? i++ != 2 ? printf("Happy Birthday to you\n") : \
+    while (i > 0) {
+        argc == 2 ? i-- != 2 ? printf("Happy Birthday to you\n") : \
         printf("Happy Birthday dear %s\n",argv[1]) : \
-        (i = 4) && printf("usage: %s name\n",argv[0]);
+        (i = -1) && printf("usage: %s name\n",argv[0]);
     }
+    return(i);
 }
