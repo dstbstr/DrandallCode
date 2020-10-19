c6425d4d6f92019966aced2e1b6470420f223069
Dustin <dstbstr17@gmail.com>
2020-10-18 20:16:17 -0700

    Made adding new files to CMake easier, enabled running the app from VSCode and making it wait for input before exiting

--- Cpp/ScratchExe/Src/Main.cpp
+++ Cpp/ScratchExe/Src/Main.cpp
@@ -2,6 +2,7 @@
 #include "Singers/NaiveSinger.h"
 
 #include <iostream>
+#include <string>
 
 int main() {
     auto singer = std::unique_ptr<IBirthdaySinger>(new NaiveSinger());
@@ -11,6 +12,8 @@ int main() {
     std::cin >> name;
     std::cout << std::endl;
 
-    std::cout << singer->Sing("Lenore");
+    std::cout << singer->Sing(name) << std::endl;
+    std::cin.ignore();
+    std::cin.ignore(); //Need two of these, one to capture the newline in the cout apparently
     return 0;
 }
