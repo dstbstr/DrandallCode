7cc3afa6406703259fd83349387a92dce17833d1
Dustin Randall <durandal@microsoft.com>
2020-10-23 14:20:56 -0700

    changed string references to string views in string utils

--- Cpp/ScratchLib/src/Utilities/StringUtilities.cpp
+++ Cpp/ScratchLib/src/Utilities/StringUtilities.cpp
@@ -2,16 +2,16 @@
 
 namespace StrUtil {
 
-    bool EqualsIgnoreCase(const std::string& lhs, const std::string& rhs) {
+    bool EqualsIgnoreCase(std::string_view lhs, std::string_view rhs) {
         static auto comparer = [](const char& c1, const char& c2) { return c1 == c2 || std::toupper(c1) == std::toupper(c2); };
 
         return (lhs.size() == rhs.size()) && std::equal(lhs.begin(), lhs.end(), rhs.begin(), comparer);
     }
 
-    std::vector<std::string> Split(std::string input, std::string delimiter) {
+    std::vector<std::string_view> Split(std::string_view input, std::string_view delimiter) {
         size_t last = 0;
         size_t next = 0;
-        std::vector<std::string> result;
+        std::vector<std::string_view> result;
 
         while ((next = input.find(delimiter, last)) != std::string::npos) {
             if (next - last > delimiter.size()) {
