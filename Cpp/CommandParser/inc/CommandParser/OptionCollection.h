d11785148c645a04b7e74c969af9913d2ee44006
Dustin <dstbstr17@gmail.com>
2020-10-27 20:20:22 -0700

    Moved Usage into OptionCollection

--- Cpp/CommandParser/inc/CommandParser/OptionCollection.h
+++ Cpp/CommandParser/inc/CommandParser/OptionCollection.h
@@ -1,43 +1,21 @@
 #ifndef __OPTIONCOLLECTION_H__
 #define __OPTIONCOLLECTION_H__
-#include "CommandParser/Option.h"
 #include "CommandParser/OptionValuePair.h"
-#include "Utilities/Format.h"
 #include <vector>
+#include <ostream>
 
 namespace CommandParser {
+    class BaseOption;
+
     class OptionCollection {
     public:
-        OptionCollection& Add(BaseOption& option) {
-            m_Options.push_back(std::move(&option));
-            return *this;
-        }
-
-        std::vector<BaseOption*> Apply(std::vector<OptionValuePair> pairs) {
-            std::vector<BaseOption*> result;
-
-            for(auto&& option : m_Options) {
-                bool found = false;
-                for(auto&& pair : pairs) {
-                    if(option->Matches(pair.Name)) {
-                        if(pair.HasValue) {
-                            option->Populate(pair.Value);
-                        }
-                        result.push_back(option);
-                        found = true;
-                        break;
-                    }
-                }
-                if(!found && option->IsRequired()) {
-                    throw std::exception(StrUtil::Format("%s option is required, but not provided", option->GetLongName()).c_str());
-                }
-            }
-
-            return result;
-        }
+        OptionCollection(std::string description);
+        OptionCollection& Add(BaseOption& option);
+        std::vector<BaseOption*> Apply(std::vector<OptionValuePair> pairs);
+        void PrintUsage(std::ostream& target) const;
     private:
-        
         std::vector<BaseOption*> m_Options;
+        std::string m_Description;
     };
 } // namespace CommandParser
 #endif // __OPTIONCOLLECTION_H__
---
+++ Cpp/CommandParser/src/OptionCollection.cpp
@@ -0,0 +1,46 @@
+#include "CommandParser/OptionCollection.h"
+#include "CommandParser/Option.h"
+#include "Utilities/Format.h"
+
+namespace CommandParser {
+    OptionCollection::OptionCollection(std::string description) : m_Description(description) {};
+
+    OptionCollection& OptionCollection::Add(BaseOption& option) {
+        m_Options.push_back(std::move(&option));
+        return *this;
+    }
+
+    void OptionCollection::PrintUsage(std::ostream& stream) const {
+        stream << m_Description << std::endl;
+        stream << "Options" << std::endl;
+        stream << "Short Name | Long Name | Required | Flag | Help Text" << std::endl;
+        stream << "----------------------------------------------------" << std::endl;
+
+        for(auto&& option : m_Options) {
+            stream << option->ToString() << std::endl;
+        }
+    }
+
+    std::vector<BaseOption*> OptionCollection::Apply(std::vector<OptionValuePair> pairs) {
+        std::vector<BaseOption*> result;
+
+        for(auto&& option: m_Options) {
+            bool found = false;
+            for(auto&& pair: pairs) {
+                if(option->Matches(pair.Name)) {
+                    if(pair.HasValue) {
+                        option->Populate(pair.Value);
+                    }
+                    result.push_back(option);
+                    found = true;
+                    break;
+                }
+            }
+            if(!found && option->IsRequired()) {
+                throw std::exception(StrUtil::Format("%s option is required, but not provided", option->GetLongName()).c_str());
+            }
+        }
+
+        return result;
+    }
+} // namespace CommandParser
