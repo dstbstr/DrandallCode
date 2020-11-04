00ec6cadf3ab5bad18dbe19a393529cd9d94329e
Dustin Randall <durandal@microsoft.com>
2020-11-04 12:43:03 -0800

    Separating out MSVC from other compilers with the Runner

---
+++ Cpp/ScratchLib/inc/Threading/Private/ClangRunner.h
@@ -0,0 +1,32 @@
+#ifndef __CLANGRUNNER_H__
+#define __CLANGRUNNER_H__
+
+#include "Threading/IRunnable.h"
+
+#include <memory>
+#include <vector>
+
+class ClangRunner {
+    {
+    public:
+        static ClangRunner Get() {
+            static ClangRunner runner;
+            return runner;
+        }
+
+        template<class ReturnType>
+        std::vector<ReturnType> RunAll(std::vector<std::unique_ptr<IRunnable<ReturnType>>> & runnables) {
+            // TODO: Implement with threading
+            std::vector<ReturnType> result;
+            result.reserve(runnables.size());
+            for(auto&& runnable: runnables) {
+                result.push_back(runnable->Execute());
+            }
+            return result;
+        }
+
+    private:
+        ClangRunner() = default;
+    };
+};
+#endif // __CLANGRUNNER_H__
---
+++ Cpp/ScratchLib/inc/Threading/Private/GnuRunner.h
@@ -0,0 +1,29 @@
+#ifndef __GNURUNNER_H__
+#define __GNURUNNER_H__
+
+#include <memory>
+#include <vector>
+
+class GnuRunner {
+public:
+    static GnuRunner Get() {
+        static GnuRunner runner;
+        return runner;
+    }
+
+    template<class ReturnType>
+    std::vector<ReturnType> RunAll(std::vector<std::unique_ptr<IRunnable<ReturnType>>>& runnables) {
+        // TODO: Implement with threading
+        std::vector<ReturnType> result;
+        result.reserve(runnables.size());
+        for(auto&& runnable: runnables) {
+            result.push_back(runnable->Execute());
+        }
+        return result;
+    }
+
+private:
+    GnuRunner() = default;
+};
+
+#endif // __GNURUNNER_H__
