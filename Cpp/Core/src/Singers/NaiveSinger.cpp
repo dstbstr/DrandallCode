6c2d17215abc4d6ce6f5ebd3f43ac4c62dedc70d
Dustin Randall <durandal@microsoft.com>
2020-12-11 10:04:38 -0800

    Removed singer stuff

--- Cpp/Core/src/Singers/NaiveSinger.cpp
+++
@@ -1,5 +0,0 @@
-#include "Singers/NaiveSinger.h"
-
-std::string NaiveSinger::Sing(std::string name) {
-    return "Happy birthday to you.  Happy birthday to you.  Happy birthday dear " + name + ".  Happy birthday to you.";
-}
