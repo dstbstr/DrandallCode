d925512f7d696f736ad2f18904f3420191db27de
Dustin Randall <durandal@microsoft.com>
2020-12-16 16:09:56 -0800

    ExpressionEvaluator and IfDefExtractor built and tested

--- Cpp/Core/src/Utilities/ExpressionEvaluator.cpp
+++ Cpp/Core/src/Utilities/ExpressionEvaluator.cpp
@@ -1,8 +1,10 @@
 #include "Utilities/ExpressionEvaluator.h"
 
+#include <regex>
 namespace {
     constexpr char TrueVal[] = "true";
     constexpr char FalseVal[] = "false";
+    static const std::regex spaceRegex(" +");
 
     std::string BoolToStr(bool val) {
         if(val) {
@@ -15,15 +17,16 @@ namespace {
         return line.find('(') != line.npos;
     }
 
-    std::string ExtractInnerExpression(const std::string& line) {
+    size_t ExtractInnerExpression(const std::string& line, std::string& outString) {
         auto closeParen = line.find_first_of(')');
         std::string substr = line.substr(0, closeParen);
         auto openParen = substr.find_last_of('(');
 
-        return line.substr(openParen + 1, closeParen - openParen);
+        outString = line.substr(openParen + 1, closeParen - openParen - 1);
+        return openParen;
     }
 
-    bool EvaluateValue(const std::string& value, std::function<bool(std::string)> predicate) {
+    bool EvaluateValue(const std::string& value, const std::function<bool(std::string)>& predicate) {
         if(value == TrueVal || value == FalseVal) {
             return value == TrueVal;
         }
@@ -33,7 +36,19 @@ namespace {
         return predicate(value);
     }
 
-    void ReplaceInnerExpression(std::string& line, std::function<bool(std::string)> predicate) {
+    bool EvaluateExpression(const std::string& line, const size_t end, const std::function<bool(std::string)>& predicate) {
+        auto symbolIndex = line.find_first_of("&|");
+        std::string lhs = line.substr(0, symbolIndex);
+        std::string rhs = line.substr(symbolIndex + 2, end - symbolIndex + 1);
+
+        switch(line[symbolIndex]) {
+        case '&': return EvaluateValue(lhs, predicate) && EvaluateValue(rhs, predicate);
+        case '|': return EvaluateValue(lhs, predicate) || EvaluateValue(rhs, predicate);
+        default: return false;
+        }
+    }
+
+    void ReplaceInnerExpression(std::string& line, const std::function<bool(std::string)>& predicate) {
         auto symbolIndex = line.find_first_of("&|");
         if(symbolIndex == line.npos) {
             line = BoolToStr(EvaluateValue(line, predicate));
@@ -44,41 +59,33 @@ namespace {
             symbolIndex = line.find_first_of("&|");
             auto nextSymbolIndex = line.find_first_of("&|", symbolIndex + 2);
             if(nextSymbolIndex == line.npos) {
-                line = BoolToStr(EvaluateValue(line, predicate));
+                line = BoolToStr(EvaluateExpression(line, line.length(), predicate));
                 return;
+            } else {
+                line = BoolToStr(EvaluateExpression(line, nextSymbolIndex, predicate)) + line.substr(nextSymbolIndex);
             }
-
-            std::string lhs = line.substr(0, symbolIndex);
-            std::string rhs = line.substr(symbolIndex + 1, nextSymbolIndex - symbolIndex + 1);
-            bool result = false;
-            switch(line[symbolIndex]) {
-            case '&': result = EvaluateValue(lhs, predicate) && EvaluateValue(rhs, predicate); break;
-            case '|': result = EvaluateValue(lhs, predicate) || EvaluateValue(lhs, predicate); break;
-            }
-
-            line = BoolToStr(result) + line.substr(nextSymbolIndex);
         }
     }
 
-    void EvaluateExpression(std::string& line, std::function<bool(std::string)> predicate) {
+    void Evaluate_Impl(std::string& line, const std::function<bool(std::string)>& predicate) {
         while(ContainsInnerExpression(line)) {
-            auto innerExpression = ExtractInnerExpression(line);
+            std::string innerExpression;
+            auto startIndex = ExtractInnerExpression(line, innerExpression);
             auto result = innerExpression;
             ReplaceInnerExpression(result, predicate);
-            line.replace(line.begin)
+            line.replace(startIndex, startIndex + innerExpression.length() + 2, result);
         }
+        ReplaceInnerExpression(line, predicate);
     }
 
 } // namespace
 
 namespace ExpressionEvaluator {
-    bool Evaluate(const std::string& line, std::function<bool(std::string)> predicate) {
+    bool Evaluate(const std::string& line, const std::function<bool(std::string)>& predicate) {
         bool result = false;
-        std::string copy = line;
-        while(ContainsInnerExpression(copy)) {
-            ReplaceInnerExpression(copy, predicate);
-        }
+        std::string copy = std::regex_replace(line, spaceRegex, ""); // remove all the spaces
 
+        Evaluate_Impl(copy, predicate);
         return EvaluateValue(copy, predicate);
     }
 } // namespace ExpressionEvaluator
