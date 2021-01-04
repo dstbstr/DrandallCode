c3250a2c184a88f3981dad06aa546933741e6c51
Dustin Randall <durandal@microsoft.com>
2021-01-04 15:13:03 -0800

    Deleting accidently added build/bin folders

--- gbell/MidiCode/Build/VS/2019/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/durandal/source/repos/DrandallCode/gbell/MidiCode/Build/VS/2019/_deps/googletest-src
-# Build directory: C:/Users/durandal/source/repos/DrandallCode/gbell/MidiCode/Build/VS/2019/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
