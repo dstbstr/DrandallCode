15ba8322d1a7b5ba59b54ac15f7a0b672374ad92
Dustin Randall <durandal@microsoft.com>
2021-01-06 08:13:49 -0800

    Removed gbellMidi from Cpp directory

--- Cpp/GbellMidi/inc/MidiOut.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MIDIOUT_H__
-#define __MIDIOUT_H__
-
-#include "libmidiclass.h"
-
-void writemidifil(int fd,mthd_hdr mc);
-
-#endif // __MIDIOUT_H__
--- Cpp/GbellMidi/inc/Utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef __UTILS_H__
-#define __UTILS_H__
-
-#include "Platform.h"
-
-int writevarlen(unsigned char *buf,unsigned long value);
-void checkargs(FILE *&fp,int &fd,int argc,char *argv[]);
-
-#endif // __UTILS_H__
