a3719c4cb11feaa7eb0461aaf94ebad0f2eea311
Dustin <dstbstr17@gmail.com>
2022-12-17 22:42:41 -0800

    2022 day 16.  Ugh

--- Cpp/AdventOfCode/inc/2022/Pressure.h
+++ Cpp/AdventOfCode/inc/2022/Pressure.h
@@ -3,19 +3,28 @@
 #include "Common.h"
 
 struct Room {
+    std::string Name = "";
     u32 FlowRate = 0;
     std::vector<std::string> Connections{};
 };
 
-using Map = std::unordered_map<std::string, Room>;
-using Graph = std::unordered_map<std::string, std::unordered_map<std::string, u32>>;
+template<size_t RoomCount, typename TDistance>
+struct State {
+    std::vector<Room> Rooms{};
+    std::unordered_map<std::string, size_t> IndexMap{};
+    
+    std::array<std::array<TDistance, RoomCount>, RoomCount> DistanceMap{};
+};
 
-void ParseLine(const std::string& line, Map& map) {
+std::vector<Room> ParseRoomList(const std::vector<std::string>& lines) {
     static const auto re = std::regex(R"(Valve (\w+) has flow rate=(\d+); tunnels? leads? to valves? ([\w, ]+))");
     std::smatch match;
+    std::vector<Room> rooms;
 
-    if (std::regex_search(line, match, re)) {
+    for (const auto& line : lines) {
+        std::regex_search(line, match, re);
         Room room;
+        room.Name = match[1].str();
         ParseNumber(match[2].str(), room.FlowRate);
         auto tunnels = match[3].str();
         auto split = StrUtil::Split(tunnels, ", ");
@@ -23,265 +32,166 @@ void ParseLine(const std::string& line, Map& map) {
             room.Connections.push_back(std::string(connection));
         }
 
-        map.insert({ match[1].str(), room });
+        rooms.push_back(room);
     }
-    else {
-        throw std::logic_error("Unable to parse " + line);
-    }
+
+    return rooms;
 }
 
-Map MakeMap(const std::vector<std::string>& lines) {
-    Map result;
-    for (const auto& line : lines) {
-        ParseLine(line, result);
+std::unordered_map<std::string, size_t> GetIndexMap(const std::vector<Room>& rooms) {
+    std::unordered_map<std::string, size_t> indexMap;
+
+    for (size_t i = 0; i < rooms.size(); i++) {
+        indexMap[rooms.at(i).Name] = i;
     }
 
-    return result;
+    return indexMap;
 }
 
-u32 FindDistance(const std::unordered_map<std::string, Room>& map, const std::string start, const std::string& end, std::unordered_set<std::string> path) {
-    auto t = map.at(start).Connections;
-    if (std::find(t.begin(), t.end(), end) != t.end()) {
-        return 1;
+template<size_t Verts, typename TDistance>
+constexpr std::array<std::array<TDistance, Verts>, Verts> GetDistanceMap(const std::vector<Room>& rooms, const std::unordered_map<std::string, size_t>& indexMap) {
+    std::array<std::array<TDistance, Verts>, Verts> result;
+
+    for (size_t i = 0; i < Verts; i++) {
+        std::fill(result.at(i).begin(), result.at(i).end(), 99);
     }
-    u32 best = 99;
-    for (auto next : t) {
-        if (path.find(next) != path.end()) continue;
 
-        path.insert(next);
-        best = std::min(best, FindDistance(map, next, end, path) + 1);
-        path.erase(next);
-    }
-    return best;
-}
+    for (size_t i = 0; i < rooms.size(); i++) {
+        auto room = rooms.at(i);
+        for (const auto& connection : room.Connections) {
+            auto a = indexMap.at(room.Name);
+            auto b = indexMap.at(connection);
 
-
-void Recurse(const std::string& current, const Map& map, Graph& graph) {
-    if (graph.contains(current)) return;
-    auto room = map.at(current);
-
-    for (auto r : room.Connections) {
-        graph[current][r] = 1;
-        graph[r][current] = 1;
-    }
-}
-
-Graph MakeGraph(const std::unordered_map<std::string, Room>& map) {
-    Graph result;
-    std::vector<std::string> names;
-
-    for (const auto& [name, room] : map) {
-        if (room.FlowRate > 0) {
-            names.push_back(name);
+            result[a][a] = 0;
+            result[b][b] = 0;
+            result[a][b] = 1;
+            result[b][a] = 1;
         }
     }
-    names.push_back("AA"); //need to keep the starting point
-
-    for (size_t i = 0; i < names.size() - 1; i++) {
-        for (size_t j = i + 1; j < names.size(); j++) {
-            std::unordered_set<std::string> path;
-            auto start = names[i];
-            auto end = names[j];
-            result[start][end] = FindDistance(map, start, end, path);
-            result[end][start] = result[start][end];
-        }
-    }
+    Constexpr::FloydWarshall(result);
 
     return result;
+
 }
 
-u32 CalculateValue(const std::vector<std::string> path, const Map& map, const Graph& graph, u32 seconds) {
-    u32 result = 0;
-    u32 remainingTime = seconds;
-
-    for (size_t i = 0; i < path.size() - 1; i++) {
-        auto pos = path[i];
-        result += map.at(pos).FlowRate * remainingTime;
-
-        auto next = path[i + 1];
-        auto distance = graph.at(pos).at(next) + 1;
-        remainingTime -= distance;
-    }
-
-    result += map.at(path[path.size() - 1]).FlowRate * remainingTime;
-    return result;
-}
-
-//Only 15 valuable valves
-//Given a number of time steps
-//Travel between all valves
-/*
-size_t CountValves(const std::unordered_map<std::string, Room>& map) {
-    std::unordered_set<std::string> valves;
-    for (const auto& [name, room] : map) {
-        valves.insert(name);
-    }
-
-    return valves.size();
-}
-
-struct Santa {
-    std::string CurrentRoom = "";
-    std::vector<std::string> OpenValves{};
-    u32 TotalSteam = 0;
-    u32 SteamPerMinute = 0;
-    std::string NextValve = "";
-    std::string NextRoom = "";
-
-    constexpr bool operator==(const Santa& other) const = default;
-
-    void Tick(const std::unordered_map<std::string, Room>& map) {
-        if (!NextValve.empty()) {
-            OpenValves.push_back(NextValve);
-            SteamPerMinute += map.at(NextValve).FlowRate;
-            NextValve.clear();
-        }
+template<size_t RoomCount, typename TDistance>
+State<RoomCount, TDistance> ParseState(const std::vector<std::string>& lines) {
+    State<RoomCount, TDistance> state;
+    state.Rooms = ParseRoomList(lines);
+    auto allIndexes = GetIndexMap(state.Rooms);
+    state.DistanceMap = GetDistanceMap<RoomCount, TDistance>(state.Rooms, allIndexes);
         
-        if (!NextRoom.empty()) {
-            CurrentRoom = NextRoom;
-            NextRoom.clear();
-        }
-
-        TotalSteam += SteamPerMinute;
-    }
-};
-
-struct Pair : Santa {
-    std::string ECurrentRoom = "";
-    std::string ENextValve = "";
-    std::string ENextRoom = "";
-
-    constexpr bool operator==(const Pair& other) const = default;
-
-    void Tick(const std::unordered_map<std::string, Room>& map) {
-        if (!NextValve.empty()) {
-            OpenValves.push_back(NextValve);
-            SteamPerMinute += map.at(NextValve).FlowRate;
-            NextValve.clear();
-        }
-        if (!ENextValve.empty()) {
-            OpenValves.push_back(ENextValve);
-            SteamPerMinute += map.at(ENextValve).FlowRate;
-            ENextValve.clear();
-        }
-
-        if (!NextRoom.empty()) {
-            CurrentRoom = NextRoom;
-            NextRoom.clear();
-        }
-        if (!ENextRoom.empty()) {
-            ECurrentRoom = ENextRoom;
-            ENextRoom.clear();
-        }
-
-        TotalSteam += SteamPerMinute;
-    }
-
-};
-
-
-struct SantaHash {
-    size_t operator()(const Santa& d) const {
-        return d.TotalSteam ^ d.SteamPerMinute + d.OpenValves.size();
-    }
-};
-
-struct PairHash {
-    size_t operator()(const Pair& p) const {
-        return p.TotalSteam ^ p.SteamPerMinute + p.OpenValves.size();
-    }
-};
-
-u32 FindBestRelease(const std::vector<std::string>& lines, u32 seconds) {
-    std::unordered_map<std::string, Room> map;
-    for (const auto& line : lines) {
-        ParseLine(line, map);
-    }
-    auto valveCount = CountValves(map);
-
-    std::vector<Santa> current;
-    std::unordered_set<Santa, SantaHash> next;
-
-    Santa initial;
-    Santa allOpened;
-
-    initial.CurrentRoom = "AA";
-    initial.OpenValves.push_back("AA"); //valve of 0, no need to come back here
-    current.push_back(initial);
-    auto sortDesc = [](const Santa& lhs, const Santa& rhs) -> bool {
-        return lhs.TotalSteam > rhs.TotalSteam;
-    };
-
-    for (u32 i = 0; i < seconds; i++) {
-        allOpened.Tick(map);
-        for (auto state : current) {
-            state.Tick(map);
-
-            if (state.OpenValves.size() == valveCount) {
-                if (allOpened.TotalSteam < state.TotalSteam) {
-                    allOpened = state;
-                }
-                continue;
-            }
-            if (state.TotalSteam < allOpened.TotalSteam) {
-                continue;
-            }
-
-            if (std::find(state.OpenValves.cbegin(), state.OpenValves.cend(), state.CurrentRoom) == state.OpenValves.cend()) {
-                auto nextState = state;
-                nextState.NextValve = nextState.CurrentRoom;
-                next.insert(nextState);
-            }
-            for (auto tunnel : map.at(state.CurrentRoom).Connections) {
-                auto nextState = state;
-                nextState.NextRoom = tunnel;
-                next.insert(nextState);
-            }
-        }
-
-        current.clear();
-        std::copy(next.begin(), next.end(), std::back_inserter(current));
-        next.clear();
-
-        //clear out the worst performers
-        size_t keepCount = 50000;
-        if (current.size() > keepCount) {
-            std::sort(current.begin(), current.end(), sortDesc);
-
-            current.erase(current.begin() + keepCount, current.end());
+    for (const auto& [name, index] : allIndexes) {
+        if (state.Rooms[index].FlowRate > 0 || name == "AA") {
+            state.IndexMap.insert({ name, index });
         }
     }
 
-    std::sort(current.begin(), current.end(), sortDesc);
-    return current[0].TotalSteam;
+    return state;
 }
-*/
 
-u32 FindBestPath(const std::vector<std::string>& lines) {
-    auto map = MakeMap(lines);
-    auto graph = MakeGraph(map);
+template<size_t RoomCount, typename TDistance>
+void RecursePermutations(const State<RoomCount, TDistance>& state, u64 seen, size_t roomIndex, s16 remainingTime, u32 totalSteam, std::unordered_map<u64, u32>& outMap) {
+    static const u64 done = [&state]() {
+        u64 result = 0;
+        for (const auto& [_, index] : state.IndexMap) {
+            result += u64(1) << index;
+        }
 
-    std::vector<std::string> path;
-    for (const auto& [key, value] : graph) {
-        if (key == "AA") continue;
-        path.push_back(key);
+        return result;
+    }();
+
+    if (remainingTime <= 0) {
+        if (outMap.contains(seen)) {
+            if (outMap.at(seen) < totalSteam) {
+                outMap.at(seen) = totalSteam;
+            }
+        }
+        else {
+            outMap[seen] = totalSteam;
+        }
+
+        return;
     }
 
-    std::sort(path.begin(), path.end());
+    totalSteam += static_cast<u32>(state.Rooms[roomIndex].FlowRate * remainingTime);
+    seen += (u64(1) << roomIndex);
+
+    if (seen == done) {
+        if (outMap.contains(seen)) {
+            if (outMap.at(seen) < totalSteam) {
+                outMap.at(seen) = totalSteam;
+            }
+        }
+        else {
+            outMap[seen] = totalSteam;
+        }
+        return;
+    }
+
+    for (const auto& [nextRoomName, nextIndex] : state.IndexMap) {
+        if (seen & (u64(1) << nextIndex)) continue;
+        auto distance = state.DistanceMap.at(roomIndex).at(nextIndex) + 1;
+        auto nextTime = remainingTime - distance;
+
+        RecursePermutations(state, seen, nextIndex, nextTime, totalSteam, outMap);
+    }
+}
+
+template<size_t RoomCount, typename TDistance>
+u32 FindBestSolo(const std::vector<std::string>& lines) {
+    auto state = ParseState<RoomCount, TDistance>(lines);
+    auto startIndex = state.IndexMap.at("AA");
+    std::unordered_map<u64, u32> permutations;
+
+    RecursePermutations<RoomCount, TDistance>(state, 0, startIndex, 30, 0, permutations);
+    std::vector<std::pair<u64, u32>> pairs;
+    for (const auto& [valves, value] : permutations) {
+        auto withoutStart = valves - (u64(1) << startIndex);
+        pairs.push_back(std::make_pair(withoutStart, value));
+    }
+
+    std::sort(pairs.begin(), pairs.end(), [](const std::pair<u64, u32>& lhs, const std::pair<u64, u32>& rhs) {
+        return lhs.second > rhs.second;
+        });
+
+    return pairs[0].second;
+}
+
+template<size_t RoomCount, typename TDistance>
+u32 FindBestPair(const std::vector<std::string>& lines) {
+    auto state = ParseState<RoomCount, TDistance>(lines);
+    auto startIndex = state.IndexMap.at("AA");
+    std::unordered_map<u64, u32> permutations;
+    
+    RecursePermutations<RoomCount, TDistance>(state, 0, startIndex, 26, 0, permutations);
+
+    std::vector<std::pair<u64, u32>> pairs;
+    for (const auto& [valves, value] : permutations) {
+        auto withoutStart = valves - (u64(1) << startIndex);
+        pairs.push_back(std::make_pair(withoutStart, value));
+    }
+
+    std::sort(pairs.begin(), pairs.end(), [](const std::pair<u64, u32>& lhs, const std::pair<u64, u32>& rhs) {
+        return lhs.second > rhs.second;
+        });
+
     u32 best = 0;
-    while (std::next_permutation(path.begin(), path.end())) {
-        path.insert(path.begin(), "AA");
-        best = std::max(best, CalculateValue(path, map, graph, 30));
-        path.erase(path.begin());
+    for (auto i = 0; i < pairs.size(); i++) {
+        for (auto j = i + 1; j < pairs.size(); j++) {
+            if ((pairs[i].first & pairs[j].first) == 0) {
+                best = std::max(best, pairs[i].second + pairs[j].second);
+            }
+        }
     }
-
     return best;
 }
+
 std::string Run(const std::vector<std::string>& lines) {
-    return ToString(FindBestPath(lines));
-    //return ToString(FindBestRelease(lines, 30));
-    //return ToString(FindBestWithPair(lines, 26));
+    //return ToString(FindBestSolo<51, u8>(lines));
+    return ToString(FindBestPair<51, u8>(lines));
 }
+
 
 bool RunTests() {
     std::vector<std::string> lines = {
@@ -297,6 +207,18 @@ bool RunTests() {
         "Valve JJ has flow rate=21; tunnel leads to valve II"
     };
 
+    //if (Find<10, u8>(lines) != 1651) return false;
+    if (FindBestSolo<10, u8>(lines) != 1651) return false;
+    //if (FindBestPair<10, u8>(lines) != 1707) return false;
+    /*
+    auto rooms = ParseRoomList(lines);
+    auto indexMap = GetIndexMap(rooms);
+    auto distances = GetDistanceMap(rooms, indexMap);
+
+    if (distances[indexMap["AA"]][indexMap["DD"]] != 1) return false;
+    if (distances[indexMap["AA"]][indexMap["CC"]] != 2) return false;
+    if (distances[indexMap["AA"]][indexMap["HH"]] != 5) return false;
+    */
     /*
     auto map = MakeMap(lines);
     auto graph = MakeGraph(map);
@@ -306,7 +228,7 @@ bool RunTests() {
     };
     if (CalculateValue(path, map, graph, 30) != 1651) return false;
     */
-    if (FindBestPath(lines) != 1651) return false;
+    //if (FindBestPath(lines) != 1651) return false;
 
     /*
     if (FindBestRelease(lines, 30) != 1651) return false;
