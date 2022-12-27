c39653546b49648aee3de95300ea9d04ee971479
Dustin <dstbstr17@gmail.com>
2022-12-26 18:02:21 -0800

    2022 complete

---
+++ Cpp/AdventOfCode/inc/2022/d24_Blizzard.h
@@ -0,0 +1,291 @@
+#pragma once
+
+#include "Common.h"
+#include "AStar.h"
+
+#include <iostream>
+
+enum struct Direction { Up, Down, Left, Right };
+
+using BlizzardDirection = std::vector<std::vector<bool>>;
+using Blizzard = std::unordered_map<Direction, BlizzardDirection>;
+
+RowCol GetStart() {
+    return { 0, 1 };
+}
+
+RowCol GetExit(const std::vector<std::string>& lines) {
+    return { lines.size() - 1, lines[lines.size() - 1].size() - 2 };
+}
+
+RowCol GetRowColCounts(const Blizzard& blizzard) {
+    auto down = blizzard.at(Direction::Down);
+    auto right = blizzard.at(Direction::Right);
+
+    return { right.size(), down[0].size()};
+}
+
+Blizzard ParseInput(const std::vector<std::string>& lines) {
+    Blizzard result;
+    BlizzardDirection up;
+    BlizzardDirection down;
+    BlizzardDirection left;
+    BlizzardDirection right;
+
+    for (const auto& line : lines) {
+        std::vector<bool> upLine;
+        std::vector<bool> downLine;
+        std::vector<bool> leftLine;
+        std::vector<bool> rightLine;
+        for (auto c : line) {
+            bool isLeft = false;
+            bool isRight = false;
+            bool isUp = false;
+            bool isDown = false;
+            switch (c) {
+            case '<': isLeft = true; break;
+            case '>': isRight = true; break;
+            case 'v': isDown = true; break;
+            case '^': isUp = true; break;
+            default: break;
+            }
+            upLine.push_back(isUp);
+            downLine.push_back(isDown);
+            leftLine.push_back(isLeft);
+            rightLine.push_back(isRight);
+        }
+
+        up.push_back(upLine);
+        down.push_back(downLine);
+        left.push_back(leftLine);
+        right.push_back(rightLine);
+    }
+
+    result[Direction::Up] = up;
+    result[Direction::Down] = down;
+    result[Direction::Left] = left;
+    result[Direction::Right] = right;
+
+    return result;
+}
+
+Blizzard GetBlizzardAtTime(const Blizzard& blizzard, u32 timeStep) {
+    Blizzard result = blizzard;
+    auto& up = result.at(Direction::Up);
+    auto& down = result.at(Direction::Down);
+    auto& left = result.at(Direction::Left);
+    auto& right = result.at(Direction::Right);
+
+    auto limits = GetRowColCounts(blizzard);
+    auto rows = limits.Row - 2;
+    auto cols = limits.Col - 2;
+    //need to skip the walls on both sides
+
+    auto yOffset = timeStep % rows;
+    auto xOffset = timeStep % cols;
+
+    for (auto row = 1; row <= rows; row++) {
+        std::rotate(left[row].begin() + 1, left[row].begin() + 1 + xOffset, left[row].end() - 1);
+        std::rotate(right[row].rbegin() + 1, right[row].rbegin() + 1 + xOffset, right[row].rend() - 1);
+    }
+
+    std::rotate(up.begin() + 1, up.begin() + 1 + yOffset, up.end() - 1);
+    std::rotate(down.rbegin() + 1, down.rbegin() + 1 + yOffset, down.rend() - 1);
+
+    return result;
+}
+
+std::vector<RowCol> GetOpenPositions(const Blizzard& blizzard) {
+    auto up = blizzard.at(Direction::Up);
+    auto down = blizzard.at(Direction::Down);
+    auto left = blizzard.at(Direction::Left);
+    auto right = blizzard.at(Direction::Right);
+
+    auto limits = GetRowColCounts(blizzard);
+
+    std::vector<RowCol> result;
+
+    for (size_t row = 1; row < limits.Row; row++) {
+        for (size_t col = 1; col < limits.Col; col++) {
+            if (!(up[row][col] || down[row][col] || left[row][col] || right[row][col])) {
+                result.push_back({ row, col });
+            }
+        }
+    }
+
+    return result;
+}
+
+void PrintBlizzard(const Blizzard& blizzard, const RowCol& santaPos) {
+    auto limits = GetRowColCounts(blizzard);
+    RowCol start = { 0, 1 };
+    RowCol end = { limits.Row - 1, limits.Col - 2 };
+
+    for (auto row = 0; row < limits.Row; row++) {
+        for (auto col = 0; col < limits.Col; col++) {
+            if (row == 0 || col == 0) {
+                std::cout << '#';
+            }
+            else if (row == limits.Row - 1 || col == limits.Col - 1) {
+                std::cout << '#';
+            }
+            else {
+                bool up = blizzard.at(Direction::Up)[row][col];
+                bool down = blizzard.at(Direction::Down)[row][col];
+                bool left = blizzard.at(Direction::Left)[row][col];
+                bool right = blizzard.at(Direction::Right)[row][col];
+                bool santa = santaPos.Row == row && santaPos.Col == col;
+
+                auto count = up + down + left + right;
+                if (count == 1) {
+                    if (santa) throw std::logic_error("Santa's standing in a blizzard");
+
+                    if (up) std::cout << '^';
+                    if (down) std::cout << 'v';
+                    if (left) std::cout << '<';
+                    if (right) std::cout << '>';
+                }
+                else if (count == 0) {
+                    if (santa) {
+                        std::cout << "S";
+                    }
+                    else {
+                        std::cout << '.';
+                    }
+                }
+                else {
+                    if (santa) throw std::logic_error("Santa's standing in at least 2 blizzards");
+
+                    std::cout << count;
+                }
+            }
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+
+std::vector<RowCol> GetNeighbors(const RowCol& pos, const RowCol& exit, const RowCol& limits) {
+    std::vector<RowCol> nextPositions;
+    if (MDistance(pos, exit) == 1) {
+        nextPositions.push_back(exit);
+    }
+
+    if (pos.Row > 1) {
+        nextPositions.push_back({ pos.Row - 1, pos.Col });
+    }
+    if (pos.Row < limits.Row - 2) {
+        nextPositions.push_back({ pos.Row + 1, pos.Col });
+    }
+    if (pos.Col > 1 && pos.Row < limits.Row - 1) {
+        nextPositions.push_back({ pos.Row, pos.Col - 1 });
+    }
+    if (pos.Col < limits.Col - 2 && pos.Row > 0 && pos.Row < limits.Row - 1) {
+        nextPositions.push_back({ pos.Row, pos.Col + 1 });
+    }
+    nextPositions.push_back(pos);
+
+    return nextPositions;
+}
+
+u32 Travel(const Blizzard& blizzard, RowCol start, RowCol exit, RowCol limits, u32 startTime) {
+    std::unordered_map<RowCol, std::vector<u32>, RowColHash> openTimePoints;
+    for (auto i = 0; i < 400; i++) {
+        auto open = GetOpenPositions(GetBlizzardAtTime(blizzard, i + startTime));
+        openTimePoints[start].push_back(i + startTime);
+        openTimePoints[exit].push_back(i + startTime);
+        for (auto pos : open) {
+            openTimePoints[pos].push_back(i + startTime);
+        }
+    }
+
+    std::vector<RowCol> current = { start };
+    std::unordered_set<RowCol, RowColHash> next;
+    std::unordered_map<RowCol, RowCol, RowColHash> moveFrom;
+
+    while (true) {
+        //std::cout << "Minute " << startTime << "\n";
+        for (const auto& pos : current) {
+            for (const auto& neighbor : GetNeighbors(pos, exit, limits)) {
+                if (neighbor == exit) {
+                    return startTime + 1;
+                }
+
+                if (std::find(openTimePoints.at(neighbor).begin(), openTimePoints.at(neighbor).end(), startTime + 1) != openTimePoints.at(neighbor).end()) {
+                    //PrintBlizzard(GetBlizzardAtTime(blizzard, startTime + 1), neighbor);
+                    if (moveFrom.find(neighbor) == moveFrom.end()) {
+                        moveFrom[neighbor] = pos;
+                    }
+                    next.insert(neighbor);
+                }
+            }
+        }
+        startTime++;
+        current.clear();
+        std::copy(next.begin(), next.end(), std::back_inserter(current));
+        next.clear();
+
+        if (current.empty()) {
+            break; //fail case
+        }
+    }
+
+    return startTime;
+
+}
+
+u32 PartOne(const std::vector<std::string>& lines) {
+    auto blizzard = ParseInput(lines);
+    auto start = GetStart();
+    auto exit = GetExit(lines);
+    auto limits = GetRowColCounts(blizzard);
+
+    return Travel(blizzard, start, exit, limits, 0);
+}
+
+u32 PartTwo(const std::vector<std::string>& lines) {
+    auto blizzard = ParseInput(lines);
+    auto start = GetStart();
+    auto exit = GetExit(lines);
+    auto limits = GetRowColCounts(blizzard);
+
+    auto result = Travel(blizzard, start, exit, limits, 0);
+    result = Travel(blizzard, exit, start, limits, result);
+    result = Travel(blizzard, start, exit, limits, result);
+    return result;
+}
+
+//690 is too low
+std::string Run(const std::vector<std::string>& lines) {
+    //return ToString(PartOne(lines));
+    return ToString(PartTwo(lines));
+}
+
+bool RunTests() {
+    std::vector<std::string> lines = {
+        "#.######",
+        "#>>.<^<#",
+        "#.<..<<#",
+        "#>v.><>#",
+        "#<^v^^>#",
+        "######.#"
+    };
+
+    //if (PartOne(lines) != 18) return false;
+    if (PartTwo(lines) != 54) return false;
+    /*
+    auto blizzard = ParseInput(lines);
+    auto start = GetStart();
+    auto exit = GetExit(lines);
+    auto limits = GetRowColCounts(blizzard);
+    */
+
+    /*
+    if (Travel(blizzard, start, exit, limits, 0) != 18) return false;
+    if (Travel(blizzard, start, exit, limits, 41) != 54) return false;
+    */
+    //if (Travel(blizzard, exit, start, limits, 18) != 41) return false;
+    //if (Travel(blizzard, exit, start, limits, 18) != 41) return false;
+
+    return true;
+}
---
+++ Cpp/AdventOfCode/inc/2022/d25_Snafu.h
@@ -0,0 +1,153 @@
+#pragma once
+
+#include "Common.h"
+
+static constexpr std::array<s64, 21> Powers = {
+    95'367'431'640'625,         // 20
+    19'073'486'328'125,         // 19
+    3'814'697'265'625,          // 18
+    762'939'453'125,            // 17
+    152'587'890'625,            // 16
+    30'517'578'125,             // 15
+    6'103'515'625,              // 14
+    1'220'703'125,              // 13
+    244'140'625,                // 12
+    48'828'125,                 // 11
+    9'765'625,                  // 10
+    1'953'125,                  // 9
+    390'625,                    // 8
+    78'125,                     // 7
+    15'625,                     // 6
+    3'125,                      // 5
+    625,                        // 4
+    125,                        // 3
+    25,                         // 2
+    5,                          // 1
+    1                           // 0
+};
+
+constexpr s64 FromSnafu(const std::string& value) {
+    s64 result = 0;
+    u64 place = 1;
+
+    for (s32 i = static_cast<s32>(value.size() - 1); i >= 0; i--) {
+        switch (value[i]) {
+        case '2': result += (2 * place); break;
+        case '1': result += place; break;
+        case '0': break;
+        case '-': result -= place; break;
+        case '=': result -= (2 * place); break;
+        default: break;
+        }
+
+        place *= 5;
+    }
+    return static_cast<s64>(result);
+}
+
+static_assert(FromSnafu("1=-0-2") == 1747);
+static_assert(FromSnafu("1121-1110-1=0") == 314159265);
+
+constexpr std::string ToSnafu(u64 value) {
+    s64 running = value;
+    int i = 0;
+    while (running < Powers[i]) {
+        i++;
+    } 
+
+    if(running > Powers[i] * 2) {
+        i--;
+    }
+
+    std::string result = "";
+    while (i < Powers.size()) {
+        auto power = Powers[i];
+        if (running < 0) {
+            if (Constexpr::Abs(running + power) > Constexpr::Abs(running)) {
+                result += "0";
+            }
+            else if (Constexpr::Abs(running + power * 2) < Constexpr::Abs(running + power)) {
+                running += power * 2;
+                result += "=";
+            }
+            else {
+                running += power;
+                result += '-';
+            }
+        }
+        else if (running > 0) {
+            if (Constexpr::Abs(running - power) > running) {
+                result += "0";
+            }
+            else if (Constexpr::Abs(running - power * 2) < Constexpr::Abs(running - power)) {
+                running -= power * 2;
+                result += "2";
+            }
+            else {
+                running -= power;
+                result += "1";
+            }
+        }
+        else {
+            result += "0";
+        }
+        i++;
+    }
+
+    if (running != 0) {
+        throw std::logic_error("Failed to reach 0");
+    }
+    return result;
+}
+
+
+static_assert(ToSnafu(1) == "1");
+static_assert(ToSnafu(2) == "2");
+static_assert(ToSnafu(3) == "1=");
+static_assert(ToSnafu(4) == "1-");
+static_assert(ToSnafu(5) == "10");
+static_assert(ToSnafu(6) == "11");
+static_assert(ToSnafu(7) == "12");
+static_assert(ToSnafu(8) == "2=");
+static_assert(ToSnafu(9) == "2-");
+static_assert(ToSnafu(10) == "20");
+static_assert(ToSnafu(1747) == "1=-0-2");
+static_assert(ToSnafu(314159265) == "1121-1110-1=0");
+
+
+std::string PartOne(const std::vector<std::string>& lines) {
+    u64 result = 0;
+    for (const auto& line : lines) {
+        result += FromSnafu(line);
+    }
+    return ToSnafu(result);
+}
+
+u32 PartTwo(const std::vector<std::string>& lines) {
+    return 0;
+}
+
+std::string Run(const std::vector<std::string>& lines) {
+    return PartOne(lines);
+}
+
+bool RunTests() {
+    std::vector<std::string> lines = {
+        "1=-0-2",
+        "12111",
+        "2=0=",
+        "21",
+        "2=01",
+        "111",
+        "20012",
+        "112",
+        "1=-1=",
+        "1-12",
+        "12",
+        "1=",
+        "122"
+    };
+
+    if (PartOne(lines) != "2=-1=0") return false;
+    return true;
+}
