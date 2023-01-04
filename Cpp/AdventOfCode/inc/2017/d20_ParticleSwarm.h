bf187a9e52e963c2d3a9d449db24e0ce832226ab
Dustin Randall <durandal@microsoft.com>
2023-01-04 13:57:47 -0800

    through day 23

---
+++ Cpp/AdventOfCode/inc/2017/d20_ParticleSwarm.h
@@ -0,0 +1,124 @@
+#pragma once
+
+#include "Common.h"
+
+struct Particle {
+    u32 Index;
+    Vec3<s64> Position;
+    Vec3<s64> Velocity;
+    Vec3<s64> Accel;
+
+    constexpr void Tick() {
+        Velocity += Accel;
+        Position += Velocity;
+    }
+};
+
+constexpr Particle ParseParticle(const std::string& line, u32 index) {
+    auto split = Constexpr::Split(line, " ");
+    auto posSplit = Constexpr::Split(split[0], ",");
+    auto velSplit = Constexpr::Split(split[1], ",");
+    auto accSplit = Constexpr::Split(split[2], ",");
+
+    Particle result;
+    result.Index = index;
+
+    Constexpr::ParseNumber(posSplit[0].substr(3), result.Position.X);
+    Constexpr::ParseNumber(posSplit[1], result.Position.Y);
+    Constexpr::ParseNumber(posSplit[2].substr(0, posSplit[2].find('>')), result.Position.Z);
+
+    Constexpr::ParseNumber(velSplit[0].substr(3), result.Velocity.X);
+    Constexpr::ParseNumber(velSplit[1], result.Velocity.Y);
+    Constexpr::ParseNumber(velSplit[2].substr(0, velSplit[2].find('>')), result.Velocity.Z);
+
+    Constexpr::ParseNumber(accSplit[0].substr(3), result.Accel.X);
+    Constexpr::ParseNumber(accSplit[1], result.Accel.Y);
+    Constexpr::ParseNumber(accSplit[2].substr(0, accSplit[2].find('>')), result.Accel.Z);
+
+    return result;
+}
+
+static_assert(ParseParticle("p=<-317,1413,1507>, v=<19,-102,-108>, a=<1,-3,-3>", 1).Position == Vec3<s64>{-317, 1413, 1507});
+static_assert(ParseParticle("p=<-317,1413,1507>, v=<19,-102,-108>, a=<1,-3,-3>", 1).Velocity == Vec3<s64>{19, -102, -108});
+static_assert(ParseParticle("p=<-317,1413,1507>, v=<19,-102,-108>, a=<1,-3,-3>", 1).Accel == Vec3<s64>{1, -3, -3});
+static_assert(ParseParticle("p=<-317,1413,1507>, v=<19,-102,-108>, a=<1,-3,-3>", 1).Index == 1);
+
+constexpr bool Collide(Particle lhs, Particle rhs) {
+    return lhs.Position == rhs.Position;
+}
+
+constexpr void RemoveCollisions(std::vector<Particle>& particles) {
+    std::vector<Vec3<s64>> seen;
+    std::vector<Vec3<s64>> toRemove;
+    for (const auto& particle : particles) {
+        if (std::find(seen.cbegin(), seen.cend(), particle.Position) == seen.cend()) {
+            seen.push_back(particle.Position);
+        }
+        else {
+            toRemove.push_back(particle.Position);
+        }
+    }
+
+    auto shouldRemove = [toRemove](const Particle& particle) {
+        return std::find(toRemove.cbegin(), toRemove.cend(), particle.Position) != toRemove.cend();
+    };
+
+    auto newEnd = std::remove_if(particles.begin(), particles.end(), shouldRemove);
+    if (newEnd != particles.end()) {
+        particles.erase(newEnd, particles.end());
+    }
+}
+
+auto PartOne(const std::vector<std::string>& lines) {
+    std::vector<Particle> particles;
+    for (u32 index = 0; index < lines.size(); index++) {
+        particles.push_back(ParseParticle(lines[index], index));
+    }
+
+    for (auto i = 0; i < 10000; i++) {
+        for (auto& particle : particles) {
+            particle.Tick();
+        }
+    }
+
+    Vec3<s64> origin{ 0, 0, 0 };
+    std::sort(particles.begin(), particles.end(), [origin](const Particle& lhs, const Particle& rhs) {
+        return MDistance(lhs.Position, origin) < MDistance(rhs.Position, origin);
+        });
+
+    return particles[0].Index;
+}
+
+auto PartTwo(const std::vector<std::string>& lines) {
+    std::vector<Particle> particles;
+    for (u32 index = 0; index < lines.size(); index++) {
+        particles.push_back(ParseParticle(lines[index], index));
+    }
+
+    for (auto i = 0; i < 10000; i++) {
+        for (auto& particle : particles) {
+            particle.Tick();
+        }
+        RemoveCollisions(particles);
+    }
+
+    return particles.size();
+}
+
+std::string Run(const std::vector<std::string>& lines) {
+    //return ToString(PartOne(lines));
+    return ToString(PartTwo(lines));
+}
+
+bool RunTests() {
+    std::vector<std::string> lines = {
+        "p=<-6,0,0>, v=<3,0,0>, a=<0,0,0>",
+        "p=<-4,0,0>, v=<2,0,0>, a=<0,0,0>",
+        "p=<-2,0,0>, v=<1,0,0>, a=<0,0,0>",
+        "p=<3,0,0>, v=<-1,0,0>, a=<0,0,0>"
+    };
+
+    if (PartTwo(lines) != 1) return false;
+
+    return true;
+}
---
+++ Cpp/AdventOfCode/inc/2017/d23_Coprocessor.h
@@ -0,0 +1,119 @@
+#pragma once
+
+#include "Common.h"
+#include <iostream>
+
+using Registers = std::array<s64, 8>;
+
+constexpr s64 ReadValue(std::string_view sv, const Registers& registers) {
+    if (sv.size() == 1 && sv[0] >= 'a') {
+        return registers[sv[0] - 'a'];
+    }
+
+    s64 result;
+    Constexpr::ParseNumber(sv, result);
+    return result;
+}
+
+constexpr void ApplyCommand(const std::string& command, s64& instruction, Registers& registers, u32& multiplyCount) {
+    auto split = Constexpr::Split(command, " ");
+    auto regIndex = split[1][0] - 'a';
+    s64 value = 0;
+    if (split.size() > 2) {
+        value = ReadValue(split[2], registers);
+    }
+
+    if (split[0] == "set") {
+        registers[regIndex] = value;
+    }
+    else if (split[0] == "add") {
+        registers[regIndex] += value;
+    }
+    else if (split[0] == "sub") {
+        registers[regIndex] -= value;
+    }
+    else if (split[0] == "mul") {
+        registers[regIndex] *= value;
+        multiplyCount++;
+    }
+    else if (split[0] == "jnz") {
+        auto compare = ReadValue(split[1], registers);
+        if (compare != 0) {
+            instruction += value - 1;
+        }
+    }
+
+    instruction++;
+}
+
+constexpr s64 PartOne(const std::vector<std::string>& lines) {
+    s64 instruction = 0;
+    Registers registers;
+    registers.fill(0);
+    u32 multiplyCount = 0;
+
+    while (instruction < static_cast<s64>(lines.size())) {
+        ApplyCommand(lines[instruction], instruction, registers, multiplyCount);
+    }
+
+    return multiplyCount;
+}
+
+auto PartTwo() {
+    auto primes = Constexpr::GetPrimes<s32>(122700);
+    s32 count = 0;
+    for (auto i = 105700; i <= 122700; i += 17) {
+        if (std::find(primes.cbegin(), primes.cend(), i) == primes.cend()) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+std::string Run(const std::vector<std::string>& lines) {
+    //return ToString(PartOne(lines));
+    (void)lines;
+    return ToString(PartTwo());
+}
+
+bool RunTests() {
+    std::vector<std::string> lines = {
+        "set f 1",
+        "set b 7",
+        "set c 58",
+        "jnz 0 2",
+        "jnz 1 5",
+        "mul b 100",
+        "sub b -100000",
+        "set c b",
+        "sub c -17000",
+        "set f 1",
+        "set d 2",
+        "set e 2",
+        "set g d",
+        "mul g e",
+        "sub g b",
+        "jnz g 2",
+        "set f 0",
+        "sub e -1",
+        "set g e",
+        "sub g b",
+        "jnz g -8",
+        "sub d -1",
+        "set g d",
+        "sub g b",
+        "jnz g -13",
+        "jnz f 2",
+        "sub h -1",
+        "set g b",
+        "sub g c",
+        "jnz g 2",
+        "jnz 1 3",
+        "sub b -17",
+        "jnz 1 -23"
+    };
+
+    //if (PartTwo(lines) != 0) return false;
+    return true;
+}
