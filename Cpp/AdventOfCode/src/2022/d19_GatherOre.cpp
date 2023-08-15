56c1348ac24e358e444ec670aefd17ea5b868b34
Dustin Randall <durandal@microsoft.com>
2023-08-15 09:55:21 -0700

    Finally got 2022/19 working quickly

--- Cpp/AdventOfCode/src/2022/d19_GatherOre.cpp
+++ Cpp/AdventOfCode/src/2022/d19_GatherOre.cpp
@@ -1,184 +1,94 @@
 #include "2022/d19_GatherOre.h"
-#include "Algorithms/AStar.h"
-
 SOLUTION(2022, 19) {
 
-    enum Mineral : u32 { Ore, Clay, Obsidian, Geode, COUNT };
+    using Entry = std::array<u32, 4>;
+    using Bp = std::vector<std::pair<Entry, Entry>>;
 
-    struct Robot {
-        Mineral Produces = Mineral::Ore;
-        std::array<u32, Mineral::COUNT> Costs{};
-    };
-
-    struct Bp {
-        u32 Index = 0;
-
-        std::array<Robot, Mineral::COUNT> Costs{};
-    };
-
-    struct Resources {
-        std::array<u32, Mineral::COUNT> Minerals{};
-        std::array<u32, Mineral::COUNT> Bots{};
-        u32 RemainingTime = 0;
-
-        constexpr void Tick() {
-            for (u32 i = 0; i < Mineral::COUNT; i++) {
-                Minerals[i] += Bots[i];
-            }
-            RemainingTime--;
-        }
-
-        constexpr bool operator==(const Resources& other) const {
-            return Minerals == other.Minerals && Bots == other.Bots && RemainingTime == other.RemainingTime;
-        }
-    };
-
-    constexpr bool CanAfford(const Robot & robot, const Resources & resources) {
-        for (u32 i = 0; i < Mineral::COUNT; i++) {
-            if (robot.Costs[i] > resources.Minerals[i]) return false;
-        }
-
-        return true;
-    }
-
-    constexpr void Purchase(const Robot & robot, Resources & resources) {
-        for (u32 i = 0; i < Mineral::COUNT; i++) {
-            resources.Minerals[i] -= robot.Costs[i];
-        }
-        resources.Bots[robot.Produces]++;
-    }
-
-    static constexpr std::array<u32, Mineral::COUNT> GetMaxCosts(const Bp & bp) {
-        std::array<u32, Mineral::COUNT> result;
-        for (auto mineral = 0; mineral < Mineral::COUNT; mineral++) {
-            u32 most = 0;
-            for (auto cost = 0; cost < Mineral::COUNT; cost++) {
-                most = std::max(most, bp.Costs[cost].Costs[mineral]);
-            }
-            result[mineral] = most;
-        }
-        return result;
-    }
-
-    //Blueprint 30: Each ore robot costs 4 ore. Each clay robot costs 3 ore. Each obsidian robot costs 2 ore and 5 clay. Each geode robot costs 2 ore and 10 obsidian.
-    constexpr Bp ParseLine(const std::string & line) {
-        auto s = Constexpr::Split(line, " ");
+    constexpr Bp ParseBlueprint(const std::string& line) {
         Bp bp;
+        auto s = Constexpr::Split(line, " ");
 
-        Constexpr::ParseNumber(s[1].substr(0, s[1].size() - 1), bp.Index);
-        Constexpr::ParseNumber(s[6], bp.Costs[Ore].Costs[Ore]);
-        Constexpr::ParseNumber(s[12], bp.Costs[Clay].Costs[Ore]);
-        Constexpr::ParseNumber(s[18], bp.Costs[Obsidian].Costs[Ore]);
-        Constexpr::ParseNumber(s[21], bp.Costs[Obsidian].Costs[Clay]);
-        Constexpr::ParseNumber(s[27], bp.Costs[Geode].Costs[Ore]);
-        Constexpr::ParseNumber(s[30], bp.Costs[Geode].Costs[Obsidian]);
+        u32 a, b, c, d, e, f;
+        Constexpr::ParseNumber(s[6], a);
+        Constexpr::ParseNumber(s[12], b);
+        Constexpr::ParseNumber(s[18], c);
+        Constexpr::ParseNumber(s[21], d);
+        Constexpr::ParseNumber(s[27], e);
+        Constexpr::ParseNumber(s[30], f);
 
-        bp.Costs[Ore].Produces = Ore;
-        bp.Costs[Clay].Produces = Clay;
-        bp.Costs[Obsidian].Produces = Obsidian;
-        bp.Costs[Geode].Produces = Geode;
+        bp.push_back(std::make_pair(Entry{ 0, 0, 0, a }, Entry{ 0, 0, 0, 1 }));
+        bp.push_back(std::make_pair(Entry{ 0, 0, 0, b }, Entry{ 0, 0, 1, 0 }));
+        bp.push_back(std::make_pair(Entry{ 0, 0, d, c }, Entry{ 0, 1, 0, 0 }));
+        bp.push_back(std::make_pair(Entry{ 0, f, 0, e }, Entry{ 1, 0, 0, 0 }));
+        bp.push_back(std::make_pair(Entry{ 0, 0, 0, 0 }, Entry{ 0, 0, 0, 0 }));
 
         return bp;
     }
 
-
-    constexpr u32 GetScore(const Bp&, const Resources & resource) {
-        auto geodes = resource.Minerals[Geode];
-        geodes += resource.Bots[Geode] * resource.RemainingTime;
-        auto obs = resource.Bots[Obsidian];
-        auto clay = resource.Bots[Clay];
-        auto ore = resource.Bots[Ore];
-
-        return geodes * 100000 + obs * 1000 + clay * 10 + ore;
+    constexpr bool all(const Entry& cost, const Entry& have) {
+        for (auto i = 0; i < cost.size(); i++) {
+            if (cost[i] > have[i]) return false;
+        }
+        return true;
     }
 
-    constexpr u32 FindMostGeodes(const Bp & bp, u32 minutes) {
-        const auto maxCosts = GetMaxCosts(bp);
+    constexpr Entry operator+(const Entry& lhs, const Entry& rhs) {
+        return { lhs[0] + rhs[0], lhs[1] + rhs[1], lhs[2] + rhs[2], lhs[3] + rhs[3] };
+    }
+    constexpr Entry operator-(const Entry& lhs, const Entry& rhs) {
+        return { lhs[0] - rhs[0], lhs[1] - rhs[1], lhs[2] - rhs[2], lhs[3] - rhs[3] };
+    }
 
-        Resources initial;
-        initial.Bots[Ore] = 1;
-        initial.RemainingTime = minutes;
+    constexpr u32 FindBfs(const Bp& bp, u32 minutes) {
+        size_t toKeep = 1000;
+        std::vector<std::pair<Entry, Entry>> todo{ std::make_pair(Entry{0, 0, 0, 0}, Entry{0, 0, 0, 1}) };
+        std::vector<std::pair<Entry, Entry>> next;
 
-        u32 minute = 1;
-        const auto toKeep = 800000;
-        auto sortResources = [&bp](const Resources& lhs, const Resources& rhs) {
-            return GetScore(bp, lhs) > GetScore(bp, rhs);
-        };
-
-        std::vector<Resources> current{ initial };
-        std::vector<Resources> next;
-
-        for (; minute <= minutes; minute++) {
-            for (auto& resource : current) {
-                for (auto mineral = 0; mineral < Mineral::COUNT; mineral++) {
-                    if (resource.Bots[mineral] >= maxCosts[mineral] && mineral != Geode) continue;
-                    if (CanAfford(bp.Costs[mineral], resource)) {
-                        Resources nextResources = resource;
-                        nextResources.Tick();
-                        Purchase(bp.Costs[mineral], nextResources);
-                        next.push_back(nextResources);
+        for (u32 minute = 1; minute <= minutes; minute++) {
+            for (auto [have, make] : todo) {
+                for (auto [cost, more] : bp) {
+                    if (all(cost, have)) {
+                        next.push_back(std::make_pair(have + make - cost, make + more));
                     }
                 }
-                resource.Tick();
-                next.push_back(resource);
             }
-
-            if (next.size() < toKeep) {
-                current = next;
+            if (next.size() > toKeep) {
+                std::sort(next.begin(), next.end(), [](const auto& lhs, const auto& rhs) {
+                    auto score1 = lhs.first + lhs.second + lhs.second;
+                    auto score2 = rhs.first + rhs.second + rhs.second;
+                    return score1 < score2;
+                    });
+                todo.clear();
+                std::copy_n(next.rbegin(), toKeep, std::back_inserter(todo));
             }
             else {
-                std::sort(next.begin(), next.end(), sortResources);
-                current.clear();
-
-                std::copy_n(next.begin(), toKeep, std::back_inserter(current));
+                todo = next;
             }
-
             next.clear();
         }
-
-        std::sort(current.begin(), current.end(), sortResources);
-        u32 best = 0;
-        for (const auto& resource : current) {
-            best = std::max(best, resource.Minerals[Geode]);
-        }
-
-        return best;
-    }
-
-    constexpr u32 SumQualityLevels(const std::vector<std::string>&lines) {
-        std::vector<Bp> bps;
-        for (const auto& line : lines) {
-            bps.push_back(ParseLine(line));
-        }
-
         u32 result = 0;
-        for (const auto& bp : bps) {
-            result += FindMostGeodes(bp, 24) * bp.Index;
+        for (const auto& [have, make] : todo) {
+            result = std::max(result, have[0]);
         }
-
         return result;
     }
-
-    constexpr u32 MultiplyQualityLevels(const std::vector<std::string>&lines) {
-        std::vector<Bp> bps;
-        for (auto i = 0; i < 3; i++) {
-            bps.push_back(ParseLine(lines[i]));
-        }
-
-        u32 result = 1;
-        for (const auto& bp : bps) {
-            result *= FindMostGeodes(bp, 32);
-        }
-
-        return result;
-    }
-
     PART_ONE() {
-        return Constexpr::ToString(SumQualityLevels(lines));
+        auto bps = ParseLines(lines, ParseBlueprint);
+        size_t result = 0;
+        for (size_t i = 0; i < bps.size(); i++) {
+            result += (FindBfs(bps[i], 24)) * (i + 1);
+        }
+        return Constexpr::ToString(result);
     }
 
     PART_TWO() {
-        return Constexpr::ToString(MultiplyQualityLevels(lines));
+        auto bps = ParseLines(lines, ParseBlueprint);
+        size_t result = 1;
+        for (size_t i = 0; i < 3; i++) {
+            result *= FindBfs(bps[i], 32);
+        }
+
+        return Constexpr::ToString(result);
     }
 
     TESTS() {
@@ -187,20 +97,22 @@ SOLUTION(2022, 19) {
             "Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."
         };
 
-        std::vector<Bp> bps{};
-        for (const auto& line : lines) {
-            bps.push_back(ParseLine(line));
-        }
+        //auto bps = ParseLines(lines, ParseBp);
+        //auto bps = ParseLines(lines, ParseBlueprint);
 
-        Resources initial{};
-        initial.Bots[Ore] = 1;
-        initial.RemainingTime = 24;
+        //auto bp = ParseBlueprint(lines[0]);
+        //if (Needs(15 * Robots[Clay], Clay, bp)) return false;
+        //if (CanAfford(5 * Robots[Geo], Geo, bp)) return false;
+
+        if (PartOne(lines) != "33") return false;
+
+        /*
         if (FindMostGeodes(bps[0], 24) != 9) return false;
         if (FindMostGeodes(bps[1], 24) != 12) return false;
 
         if (FindMostGeodes(bps[0], 32) != 56) return false;
         if (FindMostGeodes(bps[1], 32) != 62) return false;
-
+        */
         return true;
     }
 }
