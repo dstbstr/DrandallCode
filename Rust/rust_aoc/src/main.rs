6d18365b8079b742da8c87f61ae3cc5d4a54a3a4
Dustin Randall <durandal@microsoft.com>
2023-09-15 17:01:20 -0700

    Finally got multi-threading working.  2015 now runs in 500 ms instead of 2 s

--- Rust/rust_aoc/src/main.rs
+++ Rust/rust_aoc/src/main.rs
@@ -1,19 +1,17 @@
 use std::env;
 use std::fs;
-use std::process;
 use std::path::PathBuf;
+use std::process;
 use std::time::Instant;
 
 use std::collections::HashMap;
 
-//use std::thread;
-//use std::thread::JoinHandle;
-//use std::sync::{Arc, Mutex};
+use std::thread;
 
-mod year_2015;
 mod input_utils;
+mod year_2015;
 
-type Solutions = HashMap<i32, HashMap<i32, HashMap<i32, fn(&Vec<String>)->String>>>;
+type Solutions = HashMap<i32, HashMap<i32, HashMap<i32, fn(&Vec<String>) -> String>>>;
 
 fn main() {
     let mut solutions: Solutions = Solutions::new();
@@ -27,7 +25,7 @@ fn main() {
     } else {
         //run_all();
         //run_year(2015);
-        run_one(2015, 23, &solutions, true);
+        run_one(2015, 23, &solutions);
     }
     println!("");
     println!("Total Elapsed Time: {:?}", now.elapsed());
@@ -37,91 +35,77 @@ fn run_from_command_line(args: Vec<String>, solutions: Solutions) {
     match args.iter().map(|s| s.as_str()).collect::<Vec<&str>>() {
         _ if args[1] == "*" => run_all(solutions),
         _ if args.len() == 2 => run_year(args[1].parse().unwrap(), solutions),
-        _ if args.len() == 3 => run_one(args[1].parse().unwrap(), args[2].parse().unwrap(), &solutions, false),
-        _ => panic!("Unrecognized arguments")
+        _ if args.len() == 3 => run_one(
+            args[1].parse().unwrap(),
+            args[2].parse().unwrap(),
+            &solutions
+        ),
+        _ => panic!("Unrecognized arguments"),
     }
 }
 
 fn run_all(solutions: Solutions) {
-    for (year, days) in &solutions {
-        for (day, _) in days {
-            run_one(*year, *day, &solutions, false);
+    let mut pairs = vec![];
+    for (year, days) in solutions {
+        for (day, parts) in days {
+            for (_part, to_call) in parts {
+                pairs.push((read_input_file(get_file_name(year, day)), to_call));
+            }
         }
     }
-    /*
-    let shared = Arc::new(Mutex::new(solutions));
-    let temp = Arc::clone(&shared);
-    let temp2 = temp.lock().unwrap();
     let mut handles = vec![];
-
-    let years = temp2.keys().map(|y| *y);
-    for year in years {
-        let days = temp2.get(&year).unwrap().keys().map(|d| *d);
-        for day in days {
-            let s = Arc::clone(&shared);
-            handles.push(thread::spawn(move || {
-                let runner = s.lock().unwrap();
-                run_one(year, day, &runner, false);
-            }));
-        }
+    for (input, to_call) in pairs {
+        handles.push(thread::spawn(move || {
+            to_call(&input);
+        }))
     }
-
     for handle in handles {
         handle.join().unwrap();
     }
-    */
 }
 
 fn run_year(year: i32, solutions: Solutions) {
-    /*
-    let s = Arc::new(Mutex::new(solutions));
-    let temp = Arc::clone(&s);
-    let temp2 = temp.lock().unwrap();
-    let days = temp2.get(&year).unwrap().keys().map(|d| *d);
-
-    let mut handles = vec![];
-    for day in days {
-        let s = Arc::clone(&s);
-        handles.push(thread::spawn(move || { 
-            let runner = s.lock().unwrap();
-            run_one(year, day, &runner, false);
-        }));
+    let mut pairs = vec![];
+    for (y, days) in solutions {
+        if y != year {
+            continue;
+        }
+        for (day, parts) in days {
+            for (_part, to_call) in parts {
+                pairs.push((read_input_file(get_file_name(year, day)), to_call));
+            }
+        }
     }
 
+    let mut handles = vec![];
+    for (input, to_call) in pairs {
+        handles.push(thread::spawn(move || {
+            to_call(&input);
+        }))
+    }
     for handle in handles {
         handle.join().unwrap();
     }
-    */
-    
-    for(day, _) in solutions.get(&year).unwrap() {
-        run_one(year, *day, &solutions, false);
-    }
 }
 
-fn run_one(year: i32, day: i32, solutions: &Solutions, show_output: bool) {
+fn run_one(year: i32, day: i32, solutions: &Solutions) {
     let file = get_file_name(year, day);
-    let lines = read_input_file(&file);
+    let lines = read_input_file(file);
 
-    if show_output {
         println!("### {} {} ###", year, day);
-    }
 
-    for(part, func) in solutions.get(&year).unwrap().get(&day).unwrap() {
-        if show_output {
+    for (part, func) in solutions.get(&year).unwrap().get(&day).unwrap() {
             let now = Instant::now();
             println!("Part {part}: {}", func(&lines));
             println!("Elapsed Time: {:?}", now.elapsed());
-        }
-        else {
-            func(&lines);
-        }
     }
 }
 
 fn get_file_name(year: i32, day: i32) -> PathBuf {
     let mut buffer = [0; 2];
     let day_string = String::from("d") + &day.to_string() + &String::from(".txt");
-    let file = [".", "rust_aoc", "inputs", &year.to_string(), &day_string].join(std::path::MAIN_SEPARATOR.encode_utf8(&mut buffer));
+    let file = [".", "rust_aoc", "inputs", &year.to_string(), &day_string]
+        .join(std::path::MAIN_SEPARATOR.encode_utf8(&mut buffer));
     let mut result = match env::current_dir() {
         Ok(r) => r,
         Err(..) => {
@@ -129,11 +113,16 @@ fn get_file_name(year: i32, day: i32) -> PathBuf {
         }
     };
 
-    result.push(PathBuf::from(file.strip_prefix(".").unwrap().strip_prefix(std::path::MAIN_SEPARATOR).unwrap()));
+    result.push(PathBuf::from(
+        file.strip_prefix(".")
+            .unwrap()
+            .strip_prefix(std::path::MAIN_SEPARATOR)
+            .unwrap(),
+    ));
     result
 }
 
-fn read_input_file(file_name: &PathBuf) -> Vec<String> {
+fn read_input_file(file_name: PathBuf) -> Vec<String> {
     let file_content: String = match fs::read_to_string(&file_name) {
         Ok(result) => result,
         Err(..) => {
